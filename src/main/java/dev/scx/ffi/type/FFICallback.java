28fb311322e6d42fbfed392a8ee3adfcb242f822
scx567888 <scx567888@outlook.com>
2025-11-26 16:38:44 +0800

    dev

--- src/main/java/dev/scx/ffi/type/FFICallback.java
+++ src/main/java/dev/scx/ffi/type/FFICallback.java
@@ -1,8 +1,6 @@
 package dev.scx.ffi.type;
 
-// todo
-
-/// 回调函数需要继承此接口, 同时需要创建一个名为 "callback" 的方法
+/// 回调函数需要继承此接口, 同时需要创建一个名为 callbackMethodName 返回值 的方法 (默认叫做 "callback")
 ///
 /// @author scx567888
 /// @version 0.0.1
