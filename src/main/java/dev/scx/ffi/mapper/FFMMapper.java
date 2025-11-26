22e82271d47e07e662510afd211592c6f0c4a5a3
scx567888 <scx567888@outlook.com>
2025-11-26 18:14:32 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/FFMMapper.java
+++ src/main/java/dev/scx/ffi/mapper/FFMMapper.java
@@ -1,4 +1,4 @@
-package dev.scx.ffi.type;
+package dev.scx.ffi.mapper;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
@@ -7,7 +7,7 @@ import java.lang.foreign.MemorySegment;
 ///
 /// @author scx567888
 /// @version 0.0.1
-public interface FFIMapper {
+public interface FFMMapper {
 
     /// 将内部数据转换为 MemorySegment (内存段)
     ///
