cb53b2308dae8eb956c53635d3c0f65360f60a3f
scx567888 <scx567888@outlook.com>
2025-11-26 18:28:47 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/FloatArrayFFMMapper.java
+++ src/main/java/dev/scx/ffi/mapper/FloatArrayFFMMapper.java
@@ -1,33 +1,17 @@
-package dev.scx.ffi.type.primitive_array;
-
-import dev.scx.ffi.type.FFIMapper;
+package dev.scx.ffi.mapper;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
 import static java.lang.foreign.ValueLayout.JAVA_FLOAT;
 
-/// FloatArrayFFIMapper
+/// FloatArrayFFMMapper
 ///
 /// 不建议直接使用, 推荐直接使用 `float[]`
 ///
 /// @author scx567888
 /// @version 0.0.1
-public final class FloatArrayFFIMapper implements FFIMapper {
-
-    private float[] value;
-
-    public FloatArrayFFIMapper(float[] value) {
-        this.value = value;
-    }
-
-    public float[] getValue() {
-        return value;
-    }
-
-    public void setValue(float[] value) {
-        this.value = value;
-    }
+public record FloatArrayFFMMapper(float[] value) implements FFMMapper {
 
     @Override
     public MemorySegment toMemorySegment(Arena arena) {
@@ -37,7 +21,7 @@ public final class FloatArrayFFIMapper implements FFIMapper {
     @Override
     public void fromMemorySegment(MemorySegment memorySegment) {
         var temp = memorySegment.toArray(JAVA_FLOAT);
-        // 原因参考 IntArrayFFIMapper
+        // 原因参考 IntArrayFFMMapper
         System.arraycopy(temp, 0, value, 0, temp.length);
     }
 
---
+++ src/main/java/dev/scx/ffi/mapper/FloatRefFFMMapper.java
@@ -0,0 +1,28 @@
+package dev.scx.ffi.mapper;
+
+import dev.scx.ffi.type.FloatRef;
+
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+
+import static java.lang.foreign.ValueLayout.JAVA_FLOAT;
+
+/// FloatRefFFMMapper
+///
+/// 不建议直接使用, 推荐直接使用 [FloatRef]
+///
+/// @author scx567888
+/// @version 0.0.1
+public record FloatRefFFMMapper(FloatRef floatRef) implements FFMMapper {
+
+    @Override
+    public MemorySegment toMemorySegment(Arena arena) {
+        return arena.allocateFrom(JAVA_FLOAT, floatRef.getValue());
+    }
+
+    @Override
+    public void fromMemorySegment(MemorySegment memorySegment) {
+        floatRef.setValue(memorySegment.get(JAVA_FLOAT, 0));
+    }
+
+}
