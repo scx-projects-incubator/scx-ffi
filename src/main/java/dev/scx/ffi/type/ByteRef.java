c2cea9c08876ca27a9beaea9ccbee61d55643846
scx567888 <scx567888@outlook.com>
2025-11-26 17:17:28 +0800

    dev

--- src/main/java/dev/scx/ffi/type/ByteRef.java
+++ src/main/java/dev/scx/ffi/type/ByteRef.java
@@ -1,23 +1,23 @@
-package dev.scx.ffi.mapper;
+package dev.scx.ffi.type;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
 import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 
-/// ByteMapper
+/// ByteRef
 ///
 /// @author scx567888
 /// @version 0.0.1
-public class ByteMapper implements Mapper {
+public final class ByteRef implements FFIMapper {
 
     private byte value;
 
-    public ByteMapper() {
+    public ByteRef() {
         this.value = 0;
     }
 
-    public ByteMapper(byte value) {
+    public ByteRef(byte value) {
         this.value = value;
     }
 
