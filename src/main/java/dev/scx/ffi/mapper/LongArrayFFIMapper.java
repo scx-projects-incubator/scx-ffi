bf71b3b58395f26f80ea5f054e46b276e3ee642c
scx567888 <scx567888@outlook.com>
2025-11-26 17:36:31 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/LongArrayFFIMapper.java
+++ src/main/java/dev/scx/ffi/mapper/LongArrayFFIMapper.java
@@ -1,38 +1,40 @@
 package dev.scx.ffi.mapper;
 
+import dev.scx.ffi.type.FFIMapper;
+
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
-import static java.lang.foreign.ValueLayout.JAVA_SHORT;
+import static java.lang.foreign.ValueLayout.JAVA_LONG;
 
-/// ShortArrayMapper
+/// LongArrayFFIMapper
 ///
 /// @author scx567888
 /// @version 0.0.1
-public class ShortArrayMapper implements Mapper {
+public class LongArrayFFIMapper implements FFIMapper {
 
-    private short[] value;
+    private long[] value;
 
-    public ShortArrayMapper(short[] value) {
+    public LongArrayFFIMapper(long[] value) {
         this.value = value;
     }
 
-    public short[] getValue() {
+    public long[] getValue() {
         return value;
     }
 
-    public void setValue(short[] value) {
+    public void setValue(long[] value) {
         this.value = value;
     }
 
     @Override
     public MemorySegment toMemorySegment(Arena arena) {
-        return arena.allocateFrom(JAVA_SHORT, value);
+        return arena.allocateFrom(JAVA_LONG, value);
     }
 
     @Override
     public void fromMemorySegment(MemorySegment memorySegment) {
-        var temp = memorySegment.toArray(JAVA_SHORT);
+        var temp = memorySegment.toArray(JAVA_LONG);
         // 原因参考 IntArrayMapper
         System.arraycopy(temp, 0, value, 0, temp.length);
     }
--- src/main/java/dev/scx/ffi/mapper/ShortArrayFFIMapper.java
+++ src/main/java/dev/scx/ffi/mapper/ShortArrayFFIMapper.java
@@ -1,38 +1,40 @@
 package dev.scx.ffi.mapper;
 
+import dev.scx.ffi.type.FFIMapper;
+
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
-import static java.lang.foreign.ValueLayout.JAVA_LONG;
+import static java.lang.foreign.ValueLayout.JAVA_SHORT;
 
-/// LongArrayMapper
+/// ShortArrayFFIMapper
 ///
 /// @author scx567888
 /// @version 0.0.1
-public class LongArrayMapper implements Mapper {
+public class ShortArrayFFIMapper implements FFIMapper {
 
-    private long[] value;
+    private short[] value;
 
-    public LongArrayMapper(long[] value) {
+    public ShortArrayFFIMapper(short[] value) {
         this.value = value;
     }
 
-    public long[] getValue() {
+    public short[] getValue() {
         return value;
     }
 
-    public void setValue(long[] value) {
+    public void setValue(short[] value) {
         this.value = value;
     }
 
     @Override
     public MemorySegment toMemorySegment(Arena arena) {
-        return arena.allocateFrom(JAVA_LONG, value);
+        return arena.allocateFrom(JAVA_SHORT, value);
     }
 
     @Override
     public void fromMemorySegment(MemorySegment memorySegment) {
-        var temp = memorySegment.toArray(JAVA_LONG);
+        var temp = memorySegment.toArray(JAVA_SHORT);
         // 原因参考 IntArrayMapper
         System.arraycopy(temp, 0, value, 0, temp.length);
     }
