2d7022d8bbcb81013f349e8dae86faeea96e0bbc
scx567888 <scx567888@outlook.com>
2025-11-26 17:16:14 +0800

    dev

--- src/main/java/dev/scx/ffi/type/CharRef.java
+++ src/main/java/dev/scx/ffi/type/CharRef.java
@@ -1,23 +1,23 @@
-package dev.scx.ffi.mapper;
+package dev.scx.ffi.type;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
 import static java.lang.foreign.ValueLayout.JAVA_CHAR;
 
-/// CharMapper
+/// CharRef
 ///
 /// @author scx567888
 /// @version 0.0.1
-public class CharMapper implements Mapper {
+public final class CharRef implements FFIMapper {
 
     private char value;
 
-    public CharMapper() {
+    public CharRef() {
         this.value = 0;
     }
 
-    public CharMapper(char value) {
+    public CharRef(char value) {
         this.value = value;
     }
 
