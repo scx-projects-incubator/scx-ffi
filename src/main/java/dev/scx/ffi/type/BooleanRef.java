aac15c8594c1e8ce2627998b83f357bfd33b3046
scx567888 <scx567888@outlook.com>
2025-11-27 11:55:14 +0800

    dev

--- src/main/java/dev/scx/ffi/type/BooleanRef.java
+++ src/main/java/dev/scx/ffi/type/BooleanRef.java
@@ -1,6 +1,27 @@
 package dev.scx.ffi.type;
 
-//todo
+/// BooleanRef
+///
+/// @author scx567888
+/// @version 0.0.1
 public class BooleanRef {
+
+    private boolean value;
+
+    public BooleanRef() {
+        this.value = false;
+    }
+
+    public BooleanRef(boolean value) {
+        this.value = value;
+    }
+
+    public boolean getValue() {
+        return value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
 
 }
