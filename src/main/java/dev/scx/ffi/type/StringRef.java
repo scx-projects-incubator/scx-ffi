38ff55c7a06e69672a8957e61df8154b404c3355
scx567888 <scx567888@outlook.com>
2025-11-27 11:26:44 +0800

    dev

--- src/main/java/dev/scx/ffi/type/StringRef.java
+++ src/main/java/dev/scx/ffi/type/StringRef.java
@@ -1,11 +1,5 @@
 package dev.scx.ffi.type;
 
-import java.nio.charset.Charset;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-// todo 这玩意合理吗?
-
 /// StringRef
 ///
 /// @author scx567888
@@ -13,16 +7,9 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public final class StringRef {
 
     private String value;
-    private Charset charset;
 
     public StringRef(String value) {
         this.value = value;
-        this.charset = UTF_8;
-    }
-
-    public StringRef(String value, Charset charset) {
-        this.value = value;
-        this.charset = charset;
     }
 
     public String getValue() {
@@ -31,14 +18,6 @@ public final class StringRef {
 
     public void setValue(String value) {
         this.value = value;
-    }
-
-    public Charset getCharset() {
-        return charset;
-    }
-
-    public void setCharset(Charset charset) {
-        this.charset = charset;
     }
 
 }
