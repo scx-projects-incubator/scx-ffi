2d034400adada8e1f3b010c03d1d94271d8c525a
scx567888 <scx567888@outlook.com>
2025-11-27 12:47:18 +0800

    dev

--- src/main/java/dev/scx/ffi/FFMHelper.java
+++ src/main/java/dev/scx/ffi/FFMHelper.java
@@ -16,31 +16,32 @@ import static java.lang.foreign.ValueLayout.*;
 /// @version 0.0.1
 public final class FFMHelper {
 
+    // todo void 没处理
     public static MemoryLayout getMemoryLayout(Class<?> type) {
         // 1, 先处理可以直接映射的基本类型
-        if (type == Byte.class || type == byte.class) {
+        if (type == byte.class || type == Byte.class) {
             return JAVA_BYTE;
         }
-        if (type == Boolean.class || type == boolean.class) {
-            return JAVA_BOOLEAN;
-        }
-        if (type == Character.class || type == char.class) {
-            return JAVA_CHAR;
-        }
-        if (type == Short.class || type == short.class) {
+        if (type == short.class || type == Short.class) {
             return JAVA_SHORT;
         }
-        if (type == Integer.class || type == int.class) {
+        if (type == int.class || type == Integer.class) {
             return JAVA_INT;
         }
-        if (type == Long.class || type == long.class) {
+        if (type == long.class || type == Long.class) {
             return JAVA_LONG;
         }
-        if (type == Float.class || type == float.class) {
+        if (type == float.class || type == Float.class) {
             return JAVA_FLOAT;
         }
-        if (type == Double.class || type == double.class) {
+        if (type == double.class || type == Double.class) {
             return JAVA_DOUBLE;
+        }
+        if (type == boolean.class || type == Boolean.class) {
+            return JAVA_BOOLEAN;
+        }
+        if (type == char.class || type == Character.class) {
+            return JAVA_CHAR;
         }
         // 2, 处理基本类型的数组类型 这里我们使用 ADDRESS 而不使用 MemoryLayout.sequenceLayout() , 因为我们不知道数组长度
         if (type == byte[].class ||
@@ -96,11 +97,10 @@ public final class FFMHelper {
         return memoryLayouts;
     }
 
+    /// 返回值 只允许 (基本类型 | MemorySegment | FFMMapper) 三种
     public static Object convertToParameter(Object o) throws NoSuchMethodException, IllegalAccessException {
         return switch (o) {
-            //0, 空值
-            case null -> MemorySegment.NULL;
-            //1, 基本值/MemorySegment (FFM 能够直接处理, 无需转换)
+            // 1, 基本值 (FFM 能够直接处理, 无需转换)
             case Byte _,
                  Short _,
                  Integer _,
@@ -108,38 +108,42 @@ public final class FFMHelper {
                  Float _,
                  Double _,
                  Boolean _,
-                 Character _,
-                 MemorySegment _ -> o;
-            //2, 内置 Ref
-            case ByteRef r -> new ByteRefFFMMapper(r);
-            case CharRef r -> new CharRefFFMMapper(r);
-            case ShortRef r -> new ShortRefFFMMapper(r);
-            case IntRef r -> new IntRefFFMMapper(r);
-            case LongRef r -> new LongRefFFMMapper(r);
-            case FloatRef r -> new FloatRefFFMMapper(r);
-            case DoubleRef r -> new DoubleRefFFMMapper(r);
-            case StringRef r -> new StringRefFFMMapper(r);
-            //3, 字符串
-            case String s -> new StringFFMMapper(s);
-            //4, 映射类型
-            case FFMMapper m -> m;
-            //5, 结构体
-            case FFIStruct c -> new FFIStructFFMMapper(c);
-            //6, Callback 类型
-            case FFICallback c -> new FFICallbackFFMMapper(c);
-            //7, 数组类型
+                 Character _ -> o;
+            // 2, 数组类型
             case byte[] c -> new ByteArrayFFMMapper(c);
-            case char[] c -> new CharArrayFFMMapper(c);
             case short[] c -> new ShortArrayFFMMapper(c);
             case int[] c -> new IntArrayFFMMapper(c);
             case long[] c -> new LongArrayFFMMapper(c);
             case float[] c -> new FloatArrayFFMMapper(c);
             case double[] c -> new DoubleArrayFFMMapper(c);
-            default -> throw new RuntimeException("无法转换的类型 !!! " + o.getClass());
+            case char[] c -> new CharArrayFFMMapper(c);
+            // 3, 内置 Ref
+            case ByteRef r -> new ByteRefFFMMapper(r);
+            case ShortRef r -> new ShortRefFFMMapper(r);
+            case IntRef r -> new IntRefFFMMapper(r);
+            case LongRef r -> new LongRefFFMMapper(r);
+            case FloatRef r -> new FloatRefFFMMapper(r);
+            case DoubleRef r -> new DoubleRefFFMMapper(r);
+            case CharRef r -> new CharRefFFMMapper(r);
+            // 4, 字符串
+            case String s -> new StringFFMMapper(s);
+            case StringRef r -> new StringRefFFMMapper(r);
+            // 5, 内存段 (FFM 能够直接处理, 无需转换)
+            case MemorySegment _ -> o;
+            // 6, Callback 类型
+            case FFICallback c -> new FFICallbackFFMMapper(c);
+            // 7, 结构体
+            case FFIStruct c -> new FFIStructFFMMapper(c);
+            // 8, 映射类型
+            case FFMMapper m -> m;
+            // 9, 空值
+            case null -> MemorySegment.NULL;
+            default -> throw new IllegalArgumentException("无法转换的类型 !!! " + o.getClass());
         };
     }
 
     public static Object[] convertToParameters(Object[] objs) throws NoSuchMethodException, IllegalAccessException {
+        // 针对 null 做防御处理
         if (objs == null) {
             return new Object[0];
         }
@@ -148,19 +152,6 @@ public final class FFMHelper {
             result[i] = convertToParameter(objs[i]);
         }
         return result;
-    }
-
-    public static String toString(char[] buf) {
-        int len = buf.length;
-
-        for (int index = 0; index < len; index = index + 1) {
-            if (buf[index] == 0) {
-                len = index;
-                break;
-            }
-        }
-
-        return len == 0 ? "" : new String(buf, 0, len);
     }
 
 }
