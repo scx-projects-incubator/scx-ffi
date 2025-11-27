de286bccd892eca0922dcd0fabcfa3b0045c3908
scx567888 <scx567888@outlook.com>
2025-11-27 18:07:16 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/struct/NodeCreateContext.java
+++ src/main/java/dev/scx/ffi/mapper/struct/NodeCreateContext.java
@@ -1,18 +1,19 @@
 package dev.scx.ffi.mapper.struct;
 
+import dev.scx.reflect.TypeInfo;
 import java.util.ArrayList;
 import java.util.List;
 
 /// Node 创建上下文
 final class NodeCreateContext {
 
-    public List<Class<?>> chain;
+    public List<TypeInfo> chain;
 
     public NodeCreateContext() {
         this.chain = new ArrayList<>();
     }
 
-    public void startRecursionCheck(Class<?> type) {
+    public void startRecursionCheck(TypeInfo type) {
         if (this.chain.contains(type)) {
             throw new IllegalArgumentException("不支持递归嵌套结构体, type: " + type);
         }
