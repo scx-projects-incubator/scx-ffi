3ab47df49baeae7f8c19d65099fa84ddf84c0109
scx567888 <scx567888@outlook.com>
2025-11-27 18:06:32 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/struct/Node.java
+++ src/main/java/dev/scx/ffi/mapper/struct/Node.java
@@ -1,11 +1,15 @@
 package dev.scx.ffi.mapper.struct;
 
+import dev.scx.reflect.FieldInfo;
+
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
 
 sealed interface Node permits MemorySegmentNode, PrimitiveNode, StructNode {
 
     MemoryLayout createMemoryLayout();
+
+    FieldInfo fieldInfo();
 
     long writeToMemorySegment(MemorySegment memorySegment, long offset, Object value);
 
