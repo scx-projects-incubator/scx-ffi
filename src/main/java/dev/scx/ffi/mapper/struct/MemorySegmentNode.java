fd8ee3b8aab61d5807d2c7d3e9bab34917e7a179
scx567888 <scx567888@outlook.com>
2025-11-27 17:55:24 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/struct/MemorySegmentNode.java
+++ src/main/java/dev/scx/ffi/mapper/struct/MemorySegmentNode.java
@@ -1,24 +1,37 @@
 package dev.scx.ffi.mapper.struct;
 
+import dev.scx.reflect.FieldInfo;
+
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
 
 import static java.lang.foreign.ValueLayout.ADDRESS;
 
-record MemorySegmentNode(String name) implements Node {
+final class MemorySegmentNode implements Node {
+
+    private final FieldInfo fieldInfo;
+
+    public MemorySegmentNode(FieldInfo fieldInfo) {
+        this.fieldInfo = fieldInfo;
+    }
 
     @Override
     public MemoryLayout createMemoryLayout() {
-        return ADDRESS.withName(name);
+        MemoryLayout memoryLayout = ADDRESS;
+        if (fieldInfo != null) {
+            memoryLayout = memoryLayout.withName(fieldInfo.name());
+        }
+        return memoryLayout;
     }
 
     @Override
     public long writeToMemorySegment(MemorySegment memorySegment, long offset, Object value) {
         if (value instanceof MemorySegment seg) {
             memorySegment.set(ADDRESS, offset, seg);
-            return ADDRESS.byteSize(); // 返回写入的字节长度
+            return ADDRESS.byteSize();
+        } else {
+            throw new IllegalArgumentException("Expected MemorySegment value");
         }
-        throw new IllegalArgumentException("Expected MemorySegment value");
     }
 
 }
