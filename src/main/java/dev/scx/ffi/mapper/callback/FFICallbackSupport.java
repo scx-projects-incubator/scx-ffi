c0a7dbeb35c4fadede0e1537b0f2a966d2bef8ef
scx567888 <scx567888@outlook.com>
2025-11-28 13:27:26 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/callback/FFICallbackSupport.java
+++ src/main/java/dev/scx/ffi/mapper/callback/FFICallbackSupport.java
@@ -36,7 +36,7 @@ final class FFICallbackSupport {
                 throw new IllegalArgumentException("Not found any FFICallback method, name : " + callbackMethodName);
             }
             if (list.size() > 1) {
-                throw new IllegalArgumentException("found more than one FFICallback method, name : " + callbackMethodName);
+                throw new IllegalArgumentException("Found more than one FFICallback method, name : " + callbackMethodName);
             }
 
             var callbackMethod = list.get(0);
@@ -50,8 +50,8 @@ final class FFICallbackSupport {
         throw new IllegalArgumentException(ffiCallback.getClass().getName() + " is not a ClassInfo");
     }
 
-    /// 获取 Callback 参数的 内存布局 (规则和 getReturnMemoryLayout 是一致的)
-    public static MemoryLayout getParameterMemoryLayout(Class<?> type) {
+    /// 因为 参数 和 返回值支持的类型是一样的 这里抽取出来
+    public static MemoryLayout getMemoryLayout(Class<?> type) {
         // 1, 先处理可以直接映射的基本类型
         if (type == byte.class) {
             return JAVA_BYTE;
@@ -82,42 +82,25 @@ final class FFICallbackSupport {
             return ADDRESS;
         }
         // 其余全不支持 !!!
-        throw new IllegalArgumentException("不支持的 callback 参数类型 !!! " + type);
+        return null;
     }
 
-    /// 获取 Callback 返回值参数的 内存布局 (规则和 getParameterMemoryLayout 是一致的)
+    /// 获取 Callback 参数的 内存布局
+    public static MemoryLayout getParameterMemoryLayout(Class<?> type) {
+        var memoryLayout = getMemoryLayout(type);
+        if (memoryLayout == null) {
+            throw new IllegalArgumentException("不支持的 callback 参数类型 !!! " + type);
+        }
+        return memoryLayout;
+    }
+
+    /// 获取 Callback 返回值的 内存布局
     public static MemoryLayout getReturnMemoryLayout(Class<?> type) {
-        // 1, 先处理可以直接映射的基本类型
-        if (type == byte.class) {
-            return JAVA_BYTE;
+        var memoryLayout = getMemoryLayout(type);
+        if (memoryLayout == null) {
+            throw new IllegalArgumentException("不支持的 callback 返回值类型 !!! " + type);
         }
-        if (type == short.class) {
-            return JAVA_SHORT;
-        }
-        if (type == int.class) {
-            return JAVA_INT;
-        }
-        if (type == long.class) {
-            return JAVA_LONG;
-        }
-        if (type == float.class) {
-            return JAVA_FLOAT;
-        }
-        if (type == double.class) {
-            return JAVA_DOUBLE;
-        }
-        if (type == boolean.class) {
-            return JAVA_BOOLEAN;
-        }
-        if (type == char.class) {
-            return JAVA_CHAR;
-        }
-        // 2, 内存段
-        if (type == MemorySegment.class) {
-            return ADDRESS;
-        }
-        // 其余全不支持 !!!
-        throw new IllegalArgumentException("不支持的 callback 返回值类型 !!! " + type);
+        return memoryLayout;
     }
 
     public static MemoryLayout[] getParameterMemoryLayouts(Class<?>[] types) {
@@ -129,13 +112,13 @@ final class FFICallbackSupport {
     }
 
     /// 生成方法描述
-    public static FunctionDescriptor createFunctionDescriptor(Method callbackMethod) {
-        if (callbackMethod.getReturnType() == void.class) {
-            var paramLayouts = getParameterMemoryLayouts(callbackMethod.getParameterTypes());
+    public static FunctionDescriptor createFunctionDescriptor(Method method) {
+        if (method.getReturnType() == void.class) {
+            var paramLayouts = getParameterMemoryLayouts(method.getParameterTypes());
             return FunctionDescriptor.ofVoid(paramLayouts);
         } else {
-            var returnLayout = getReturnMemoryLayout(callbackMethod.getReturnType());
-            var paramLayouts = getParameterMemoryLayouts(callbackMethod.getParameterTypes());
+            var returnLayout = getReturnMemoryLayout(method.getReturnType());
+            var paramLayouts = getParameterMemoryLayouts(method.getParameterTypes());
             return FunctionDescriptor.of(returnLayout, paramLayouts);
         }
     }
