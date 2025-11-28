aa4646a3d91542047350f00b3896a0fe90babd4c
scx567888 <scx567888@outlook.com>
2025-11-28 17:51:36 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/struct/Node.java
+++ src/main/java/dev/scx/ffi/mapper/struct/Node.java
@@ -5,8 +5,7 @@ import dev.scx.reflect.FieldInfo;
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
 
-///
-sealed interface Node permits MemorySegmentNode, PrimitiveNode, StructNode {
+sealed interface Node permits PrimitiveNode, MemorySegmentNode, StructNode {
 
     /// 创建一个内存布局
     MemoryLayout createMemoryLayout();
@@ -14,10 +13,10 @@ sealed interface Node permits MemorySegmentNode, PrimitiveNode, StructNode {
     /// fieldInfo
     FieldInfo fieldInfo();
 
-    /// 将 value 写入 MemorySegment
-    long writeToMemorySegment(MemorySegment memorySegment, long offset, Object value) throws IllegalAccessException;
+    /// 将 targetObject 写入到 memorySegment 中, 注意 不是直接读取 targetObject 本体, 而是读取 targetObject 的字段.
+    long writeToMemorySegment(MemorySegment memorySegment, long offset, Object targetObject) throws IllegalAccessException;
 
-    /// 将 memorySegment 反射读取到 targetObject 中, 注意 不是直接修改 targetObject 本体而是修改 targetObject 的字段
+    /// 将 memorySegment 读取到 targetObject 中, 注意 不是直接写入 targetObject 本体, 而是写入 targetObject 的字段.
     long readFromMemorySegment(MemorySegment memorySegment, long offset, Object targetObject) throws IllegalAccessException;
 
 }
