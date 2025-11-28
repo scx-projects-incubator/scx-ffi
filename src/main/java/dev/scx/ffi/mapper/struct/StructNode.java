a917ddea860eb82bf708543266a79f5a51987a58
scx567888 <scx567888@outlook.com>
2025-11-28 17:54:09 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/struct/StructNode.java
+++ src/main/java/dev/scx/ffi/mapper/struct/StructNode.java
@@ -5,30 +5,24 @@ import dev.scx.reflect.FieldInfo;
 
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
-import java.util.ArrayList;
-import java.util.List;
 
 final class StructNode implements Node {
 
     private final FieldInfo fieldInfo;
+    private final Node[] fieldNodes;
     private final ClassInfo classInfo;
-    private final List<Node> fieldNodes;
 
-    public StructNode(FieldInfo fieldInfo, ClassInfo classInfo) {
+    public StructNode(FieldInfo fieldInfo, Node[] fieldNodes, ClassInfo classInfo) {
         this.fieldInfo = fieldInfo;
+        this.fieldNodes = fieldNodes;
         this.classInfo = classInfo;
-        this.fieldNodes = new ArrayList<>();
-    }
-
-    public void addFieldNode(Node fieldNode) {
-        fieldNodes.add(fieldNode);
     }
 
     @Override
     public MemoryLayout createMemoryLayout() {
-        var arr = new MemoryLayout[fieldNodes.size()];
-        for (int i = 0; i < fieldNodes.size(); i = i + 1) {
-            arr[i] = fieldNodes.get(i).createMemoryLayout();
+        var arr = new MemoryLayout[fieldNodes.length];
+        for (int i = 0; i < fieldNodes.length; i = i + 1) {
+            arr[i] = fieldNodes[i].createMemoryLayout();
         }
         var memoryLayout = MemoryLayout.structLayout(arr);
         if (fieldInfo != null) {
@@ -43,12 +37,16 @@ final class StructNode implements Node {
     }
 
     @Override
-    public long writeToMemorySegment(MemorySegment memorySegment, long offset, Object value) throws IllegalAccessException {
+    public long writeToMemorySegment(MemorySegment memorySegment, long offset, Object targetObject) throws IllegalAccessException {
         long totalBytesWritten = 0;
         for (var fieldNode : fieldNodes) {
-            // 这里不需要对 fieldInfo 判空, 因为 必定有值.
-            var fieldValue = fieldNode.fieldInfo().get(value);
-            totalBytesWritten += fieldNode.writeToMemorySegment(memorySegment, offset + totalBytesWritten, fieldValue);
+            var subObject = targetObject;
+            // 如果是 StructNode 我们 使用子对象.
+            if (fieldNode instanceof StructNode) {
+                // 这里不需要对 fieldInfo 判空, 因为 必定有值.
+                subObject = fieldNode.fieldInfo().get(targetObject);
+            }
+            totalBytesWritten += fieldNode.writeToMemorySegment(memorySegment, offset + totalBytesWritten, subObject);
         }
         return totalBytesWritten;
     }
@@ -57,12 +55,13 @@ final class StructNode implements Node {
     public long readFromMemorySegment(MemorySegment memorySegment, long offset, Object targetObject) throws IllegalAccessException {
         long totalBytesWritten = 0;
         for (var fieldNode : fieldNodes) {
-            // 如果是 StructNode 我们 写入子对象.
-            if (fieldNode instanceof StructNode structNode) {
+            var subObject = targetObject;
+            // 如果是 StructNode 我们 使用子对象.
+            if (fieldNode instanceof StructNode) {
                 // 这里不需要对 fieldInfo 判空, 因为 必定有值.
-                targetObject = fieldNode.fieldInfo().get(targetObject);
+                subObject = fieldNode.fieldInfo().get(targetObject);
             }
-            totalBytesWritten += fieldNode.readFromMemorySegment(memorySegment, offset + totalBytesWritten, targetObject);
+            totalBytesWritten += fieldNode.readFromMemorySegment(memorySegment, offset + totalBytesWritten, subObject);
         }
         return totalBytesWritten;
     }
