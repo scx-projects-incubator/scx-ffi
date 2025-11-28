da4dd798e92afa213cceaa1f9ff6691ffe51e1b1
scx567888 <scx567888@outlook.com>
2025-11-28 13:03:37 +0800

    dev

--- src/main/java/dev/scx/ffi/type/FFIStruct.java
+++ src/main/java/dev/scx/ffi/type/FFIStruct.java
@@ -6,15 +6,13 @@ import java.lang.foreign.MemorySegment;
 
 /// 结构数据需要继承此接口 以便在处理时按照结构数据的方式来处理
 ///
-/// ### 关于内存布局
-///
-/// - 默认使用 字段声明顺序, 如果需要自定义 请使用 [LayoutOrder] 注解.
-///
 /// ### 支持的 字段类型
-///
 /// - 1. 基本类型: `byte`, `short`, `int`, `long`, `float`, `double`, `boolean`, `char`.
 /// - 2. 内存段: [MemorySegment].
 /// - 3. 结构体: [FFIStruct].
+///
+/// ### 关于内存布局
+/// - 默认使用 字段声明顺序, 如果需要自定义 请使用 [LayoutOrder] 注解.
 ///
 /// @author scx567888
 /// @version 0.0.1
