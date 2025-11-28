7df9afa3bbe3070a74c8cfff8de1efa75ca55b93
scx567888 <scx567888@outlook.com>
2025-11-28 18:48:22 +0800

    dev

--- src/main/java/dev/scx/ffi/mapper/string/EncodedStringFFMMapper.java
+++ src/main/java/dev/scx/ffi/mapper/string/EncodedStringFFMMapper.java
@@ -1,26 +1,25 @@
 package dev.scx.ffi.mapper.string;
 
 import dev.scx.ffi.mapper.FFMMapper;
-import dev.scx.ffi.type.StringRef;
+import dev.scx.ffi.type.EncodedString;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
-// todo 这个类的可用性有问题
-/// StringRefFFMMapper
+/// EncodedStringFFMMapper
 ///
 /// @author scx567888
 /// @version 0.0.1
-public record StringRefFFMMapper(StringRef stringRef) implements FFMMapper {
+public record EncodedStringFFMMapper(EncodedString encodedString) implements FFMMapper {
 
     @Override
     public MemorySegment toMemorySegment(Arena arena) {
-        return arena.allocateFrom(stringRef.getValue(), stringRef.getCharset());
+        return arena.allocateFrom(encodedString.value(), encodedString.charset());
     }
 
     @Override
     public void fromMemorySegment(MemorySegment memorySegment) {
-        stringRef.setValue(memorySegment.getString(0, stringRef.getCharset()));
+        // 只读的 这里忽略
     }
 
 }
