e51d7a66d2070f820e5992852f122d523f7d6c79
scx567888 <scx567888@outlook.com>
2025-11-28 13:00:47 +0800

    dev

--- src/main/java/dev/scx/ffi/type/FFICallback.java
+++ src/main/java/dev/scx/ffi/type/FFICallback.java
@@ -4,8 +4,11 @@ import java.lang.foreign.MemorySegment;
 
 /// 回调函数需要继承此接口, 同时需要创建一个名为 callbackMethodName 返回值 的方法 (默认叫做 "callback")
 ///
-/// ### 回调函数支持的参数和返回值类型.
+/// ### 回调函数支持的参数类型.
+/// - 1. 基本类型: `byte`, `short`, `int`, `long`, `float`, `double`, `boolean`, `char`.
+/// - 2. 内存段: [MemorySegment].
 ///
+/// ### 回调函数支持的返回值类型.
 /// - 1. 基本类型: `byte`, `short`, `int`, `long`, `float`, `double`, `boolean`, `char`.
 /// - 2. 内存段: [MemorySegment].
 ///
