63e68e009a9a1a581c9b18299426420c595b6c9b
scx567888 <scx567888@outlook.com>
2025-11-28 14:50:53 +0800

    dev

--- src/test/java/dev/scx/ffi/test/CTest.java
+++ src/test/java/dev/scx/ffi/test/CTest.java
@@ -4,11 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static dev.scx.ffi.test.platform.c.C.C;
+import static java.lang.foreign.ValueLayout.JAVA_INT;
 
 public class CTest {
 
     public static void main(String[] args) {
         test1();
+        test2();
     }
 
     @Test
@@ -31,6 +33,19 @@ public class CTest {
         var d1 = Math.sqrt(88);
         Assert.assertEquals(d, d1);
 
+    }
+
+    @Test
+    public static void test2() {
+        var array = new int[]{2, 5, 7, 1, 4, 56, 12, 31, 99999, 90, 271, 2};
+
+        C.qsort(array, array.length, Integer.BYTES, (aAddr, bAddr) -> {
+            int a = aAddr.get(JAVA_INT, 0);
+            int b = bAddr.get(JAVA_INT, 0);
+            return Integer.compare(a, b);
+        });
+
+        Assert.assertEquals(array, new int[]{1, 2, 2, 4, 5, 7, 12, 31, 56, 90, 271, 99999});
     }
 
 }
